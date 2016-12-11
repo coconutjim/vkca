a4d0e9946f689ae12647a3ad55884155d528c9e9
luo <luo@idp-crew.com>
2016-12-11 08:03:20 +0300

    added collecting user info method

--- domain/user.py
+++ domain/user.py
@@ -3,10 +3,16 @@ __author__ = 'Lev'
 
 class User:
 
-    def __init__(self, user_id, full_name, birth_date, gender, city, reg_date):
+    def __init__(self, user_id, full_name, birth_date, gender, city, country, reg_date):
         self.user_id = user_id
         self.full_name = full_name
         self.birth_date = birth_date
         self.gender = gender
         self.city = city
+        self.country = country
         self.reg_date = reg_date
+
+    def __repr__(self):
+        return '<User uid:{} name:{} bdate:{} gender:{} city:{} country:{} rdate:{}>'.\
+            format(self.user_id, self.full_name, self.birth_date, self.gender,
+                   self.city, self.country, self.reg_date)
--- vk_api/searching.py
+++ vk_api/searching.py
@@ -1,5 +1,33 @@
 __author__ = 'Lev'
 import requests
 import json
+import re
+from datetime import datetime
 
-#from
+from domain.user import User
+from settings import VK_SEARCHING_TOKEN
+from util import log
+
+gender_dict = {0: None, 1: 'F', 2: 'M'}
+date_pattern = '\d+\.\d+\.\d+'
+
+
+def get_user_info(user_id, date):
+    log('getting info for {}'.format(user_id))
+    method_url = 'https://api.vk.com/method/users.get?'
+    params = dict(access_token=VK_SEARCHING_TOKEN, user_ids=user_id, fields='bdate,sex,city,country')
+    response = requests.get(method_url, params=params)
+    result = json.loads(response.text)
+    if 'error' in result:
+        error = 'searching error({}): {}'.format(result['error']['error_code'], result['error']['error_msg'])
+        raise Exception(error)
+    result = result['response'][0]
+    name = result['first_name'] + ' ' + result['last_name']
+    bdate = None
+    if re.match(date_pattern, result['bdate']) is not None:
+        dt = datetime.strptime(result['bdate'], '%d.%m.%Y')
+        bdate = int((dt - datetime(1970, 1, 1)).total_seconds())
+    gender = gender_dict[result['sex']]
+    city = result['city']  # TODO: consider
+    country = result['country']  # TODO: consider
+    return User(user_id, name, bdate, gender, city, country, date)
