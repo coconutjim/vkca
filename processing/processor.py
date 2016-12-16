167df9d5497d91c6e25a979404892e295e92f8c2
luo <luo@idp-crew.com>
2016-12-16 06:20:30 +0300

    unified and refactored the processor

--- processing/processor.py
+++ processing/processor.py
@@ -48,97 +48,127 @@ help_text_eng = 'You are dealing with Content Aggregator. List of available comm
                'Help\n' \
                'Mind that request length should be from 1 up to 255 symbols'
 
+help_text_dict = dict(ru=help_text_ru, eng=help_text_eng)
 
 news_cats_dict_eng = dict(politics='Politics', economics='Economics', incidents='Incidents', sport='Sport',
-                      science='Science', culture='Culture', religion='Religion')
-news_cats_dict_ru = {u'политика':'Politics', u'экономика': 'Economics', u'происшествия':'Incidents',
-                     u'спорт':'Sport', u'наука':'Science', u'культура': 'Culture', u'религия':'Religion'}
+                          science='Science', culture='Culture', religion='Religion')
+news_cats_dict_ru = {u'политика': 'Politics', u'экономика': 'Economics', u'происшествия':'Incidents',
+                     u'спорт': 'Sport', u'наука': 'Science', u'культура': 'Culture', u'религия': 'Religion'}
+
+news_cats_dict = dict(ru=news_cats_dict_ru, eng=news_cats_dict_eng)
 
 
-def parse_eng_request(req, text):
+stopwords_dict_eng = dict(help='help', news='news', weather='weather', hourly='hourly', currencies='currencies',
+                          music='music', video='video', locale='change language')
+
+stopwords_dict_ru = dict(help=u'помощь', news=u'новости', weather=u'погода', hourly=u'почасовая', currencies=u'валюты',
+                         music=u'музыка', video=u'видео', locale=u'смена языка')
+
+stopwords_dict = dict(ru=stopwords_dict_ru, eng=stopwords_dict_eng)
+
+quotes_message_dict = dict(ru=u'Пожалуйста, проверьте правильность написания команды. Запрос должен быть в кавычках',
+                           eng='Please check request correctness. Query must be in quotes')
+
+not_found_message_dict = dict(ru=u'К сожалению, по запросу "{}" ничего не найдено', eng='Nothing found by "{}"')
+
+change_locale_message_dict = dict(ru='Done. Your language is now English', eng=u'Выполнено. Язык сменен на русский')
+
+locale_to_change_dict = dict(ru='eng', eng='ru')
+
+unknown_req_message_dict = dict(ru=u'Неизвестный тип запроса. Попробуйте вызвать команду "помощь"',
+                                eng='Unknown request. Try typing "help" command')
+
+processing_err_message_dict = dict(ru=u'Ошибка в выполнении запроса',
+                                   eng='Error in processing request')
+
+
+def parse_request(req, text, locale='ru'):
+    sws = stopwords_dict[locale]
     try:
-        if 'help' in text:
+        if sws['help'] in text:
             req.category = 'Help'
             req.type = 'Help'
-            req.response_text = help_text_eng
+            req.response_text = help_text_dict[locale]
             req.success = 1
             req.complete = send_plain_message(req.user_id, req.response_text)
             req.save = True
             answers_queue.put(req)
             return
-        if 'news' in text:
+        if sws['news'] in text:
             req.category = 'News'
             req.save = True
-
             if text.count('&quot;') == 2:
                 found = re.findall('&quot;([^"]*)&quot;', text)
                 if found is None or len(found) == 0 or found[0] == '':
                     req.type = 'Query'
                     req.success = 0
-                    req.response_text = 'Please check request correctness. Query must be in quotes'
+                    req.response_text = quotes_message_dict[locale]
+                    req.error_message = 'incorrect query'
                     req.complete = send_plain_message(req.user_id, req.response_text)
                     answers_queue.put(req)
                     return
                 query = found[0]
-                if query in news_cats_dict_eng:
-                    req.type = news_cats_dict_eng[query]
+                cats_dict = news_cats_dict[locale]
+                if query in cats_dict:
+                    req.type = cats_dict[query]
                     req.success = 1
-                    req.response_text = news_by_category(req.type, locale='eng')
+                    req.response_text = news_by_category(req.type, locale=locale)
                     req.complete = send_plain_message(req.user_id, req.response_text)
                     answers_queue.put(req)
                     return
                 req.type = 'Query'
                 req.success = 1
-                res = news_by_query(query, locale='eng')
+                res = news_by_query(query, locale=locale)
                 if res == '':
-                    res = 'Nothing found by "{}"'.format(query)
+                    res = not_found_message_dict[locale].format(query)
                 req.response_text = res
                 req.complete = send_plain_message(req.user_id, req.response_text)
                 answers_queue.put(req)
                 return
 
             req.type = 'All'
-            req.response_text = default_news(locale='eng')
-
+            req.response_text = default_news(locale=locale)
             req.complete = send_plain_message(req.user_id, req.response_text)
             answers_queue.put(req)
             return
-        if 'weather' in text:
+        if sws['weather'] in text:
             req.category = 'Weather'
-            if 'hourly' in text:
+            if sws['hourly'] in text:
                 req.type = 'Hourly'
-                req.response_text = hourly_weather(locale='eng')
+                req.response_text = hourly_weather(locale=locale)
             else:
                 req.type = 'Next'
-                req.response_text = default_weather(locale='eng')
+                req.response_text = default_weather(locale=locale)
             req.success = 1
             req.complete = send_plain_message(req.user_id, req.response_text)
             req.save = True
             answers_queue.put(req)
             return
-        if 'currencies' in text:
+        if sws['currencies'] in text:
             req.category = 'Finance'
             req.type = 'Currencies'
-            req.response_text = default_currencies(locale='eng')
+            req.response_text = default_currencies(locale=locale)
             req.success = 1
             req.complete = send_plain_message(req.user_id, req.response_text)
             req.save = True
             answers_queue.put(req)
             return
-        if 'music' in text:
+        if sws['music'] in text:
             req.category = 'Music'
             req.type = 'Query'
             req.save = True
             if text.count('&quot;') != 2:
                 req.success = 0
-                req.response_text = 'Please check request correctness. Query must be in quotes'
+                req.response_text = quotes_message_dict[locale]
+                req.error_message = 'incorrect query'
                 req.complete = send_plain_message(req.user_id, req.response_text)
                 answers_queue.put(req)
                 return
             found = re.findall('&quot;([^"]*)&quot;', text)
             if found is None or len(found) == 0 or found[0] == '':
                 req.success = 0
-                req.response_text = 'Please check request correctness. Query must be in quotes'
+                req.response_text = quotes_message_dict[locale]
+                req.error_message = 'empty query'
                 req.complete = send_plain_message(req.user_id, req.response_text)
                 answers_queue.put(req)
                 return
@@ -146,27 +176,29 @@ def parse_eng_request(req, text):
             req.success = 1
             music = music_by_query(query)
             if music is None:
-                req.response_text = 'Nothing found by "{}"'.format(query)
+                req.response_text = not_found_message_dict[locale].format(query)
                 req.complete = send_plain_message(req.user_id, req.response_text)
             else:
                 req.response_text = music
                 req.complete = send_message_music(req.user_id, '', music)
             answers_queue.put(req)
             return
-        if 'video' in text:
+        if sws['video'] in text:
             req.category = 'Video'
             req.type = 'Query'
             req.save = True
             if text.count('&quot;') != 2:
                 req.success = 0
-                req.response_text = 'Please check request correctness. Query must be in quotes'
+                req.response_text = quotes_message_dict[locale]
+                req.error_message = 'incorrect query'
                 req.complete = send_plain_message(req.user_id, req.response_text)
                 answers_queue.put(req)
                 return
             found = re.findall('&quot;([^"]*)&quot;', text)
             if found is None or len(found) == 0 or found[0] == '':
                 req.success = 0
-                req.response_text = 'Please check request correctness. Query must be in quotes'
+                req.response_text = quotes_message_dict[locale]
+                req.error_message = 'empty query'
                 req.complete = send_plain_message(req.user_id, req.response_text)
                 answers_queue.put(req)
                 return
@@ -174,18 +206,18 @@ def parse_eng_request(req, text):
             req.success = 1
             video = video_by_query(query)
             if video is None:
-                req.response_text = 'Nothing found by "{}"'.format(query)
+                req.response_text = not_found_message_dict[locale].format(query)
                 req.complete = send_plain_message(req.user_id, req.response_text)
             else:
                 req.response_text = video
                 req.complete = send_message_video(req.user_id, '', video)
             answers_queue.put(req)
             return
-        if 'change language' in text:
+        if sws['locale'] in text:
             conn = None
             try:
                 conn = config.MYSQL_POOL.get_connection()
-                update_user_locale(conn, req.user_id, 'ru')
+                update_user_locale(conn, req.user_id, locale_to_change_dict[locale])
             except Exception as e:
                 log(str(e))
                 return
@@ -194,7 +226,7 @@ def parse_eng_request(req, text):
                     conn.close()
             req.category = 'Locale'
             req.type = 'Change'
-            req.response_text = u'Выполнено. Язык сменен на русский'
+            req.response_text = change_locale_message_dict[locale]
             req.success = 1
             req.complete = send_plain_message(req.user_id, req.response_text)
             req.save = True
@@ -202,7 +234,7 @@ def parse_eng_request(req, text):
             return
         req.category = 'Undefined'
         req.type = 'Undefined'
-        req.response_text = 'Unknown request. Try typing "help" command'
+        req.response_text = unknown_req_message_dict[locale]
         req.success = 0
         req.complete = send_plain_message(req.user_id, req.response_text)
         req.error_message = 'undefined request'
@@ -212,175 +244,7 @@ def parse_eng_request(req, text):
         log(str(e))
         req.success = 0
         req.error_message = 'error in processing request'
-        req.response_text = 'Error in processing request'
-        req.complete = send_plain_message(req.user_id, req.answer_text)
-        req.save = True
-        answers_queue.put(req)
-
-
-def parse_ru_request(req, text):
-    try:
-        if u'помощь' in text:
-            req.category = 'Help'
-            req.type = 'Help'
-            req.response_text = help_text_ru
-            req.success = 1
-            req.complete = send_plain_message(req.user_id, req.response_text)
-            req.save = True
-            answers_queue.put(req)
-            return
-        if u'новости' in text:
-            req.category = 'News'
-            req.save = True
-
-            if text.count('&quot;') == 2:
-                found = re.findall('&quot;([^"]*)&quot;', text)
-                if found is None or len(found) == 0 or found[0] == '':
-                    req.type = 'Query'
-                    req.success = 0
-                    req.response_text = u'Пожалуйста, проверьте правильность написания команды. ' \
-                                        u'Запрос должен быть в кавычках'
-                    req.complete = send_plain_message(req.user_id, req.response_text)
-                    answers_queue.put(req)
-                    return
-                query = found[0]
-                if query in news_cats_dict_ru:
-                    req.type = news_cats_dict_ru[query]
-                    req.success = 1
-                    req.response_text = news_by_category(req.type)
-                    req.complete = send_plain_message(req.user_id, req.response_text)
-                    answers_queue.put(req)
-                    return
-                req.type = 'Query'
-                req.success = 1
-                res = news_by_query(query)
-                if res == '':
-                    res = u'К сожалению, по запросу "{}" ничего не найдено'.format(query)
-                req.response_text = res
-                req.complete = send_plain_message(req.user_id, req.response_text)
-                answers_queue.put(req)
-                return
-
-            req.type = 'All'
-            req.response_text = default_news()
-
-            req.complete = send_plain_message(req.user_id, req.response_text)
-            answers_queue.put(req)
-            return
-        if u'погода' in text:
-            req.category = 'Weather'
-            if u'почасовая' in text:
-                req.type = 'Hourly'
-                req.response_text = hourly_weather()
-            else:
-                req.type = 'Next'
-                req.response_text = default_weather()
-            req.success = 1
-            req.complete = send_plain_message(req.user_id, req.response_text)
-            req.save = True
-            answers_queue.put(req)
-            return
-        if u'валюты' in text:
-            req.category = 'Finance'
-            req.type = 'Currencies'
-            req.response_text = default_currencies()
-            req.success = 1
-            req.complete = send_plain_message(req.user_id, req.response_text)
-            req.save = True
-            answers_queue.put(req)
-            return
-        if u'музыка' in text:
-            req.category = 'Music'
-            req.type = 'Query'
-            req.save = True
-            if text.count('&quot;') != 2:
-                req.success = 0
-                req.response_text = u'Пожалуйста, проверьте правильность написания команды. ' \
-                                    u'Запрос должен быть в кавычках'
-                req.complete = send_plain_message(req.user_id, req.response_text)
-                answers_queue.put(req)
-                return
-            found = re.findall('&quot;([^"]*)&quot;', text)
-            if found is None or len(found) == 0 or found[0] == '':
-                req.success = 0
-                req.response_text = u'Пожалуйста, проверьте правильность написания команды. ' \
-                                    u'Запрос должен быть в кавычках'
-                req.complete = send_plain_message(req.user_id, req.response_text)
-                answers_queue.put(req)
-                return
-            query = found[0]
-            req.success = 1
-            music = music_by_query(query)
-            if music is None:
-                req.response_text = u'К сожалению, по запросу "{}" ничего не найдено'.format(query)
-                req.complete = send_plain_message(req.user_id, req.response_text)
-            else:
-                req.response_text = music
-                req.complete = send_message_music(req.user_id, '', music)
-            answers_queue.put(req)
-            return
-        if u'видео' in text:
-            req.category = 'Video'
-            req.type = 'Query'
-            req.save = True
-            if text.count('&quot;') != 2:
-                req.success = 0
-                req.response_text = u'Пожалуйста, проверьте правильность написания команды. ' \
-                                    u'Запрос должен быть в кавычках'
-                req.complete = send_plain_message(req.user_id, req.response_text)
-                answers_queue.put(req)
-                return
-            found = re.findall('&quot;([^"]*)&quot;', text)
-            if found is None or len(found) == 0 or found[0] == '':
-                req.success = 0
-                req.response_text = u'Пожалуйста, проверьте правильность написания команды. ' \
-                                    u'Запрос должен быть в кавычках'
-                req.complete = send_plain_message(req.user_id, req.response_text)
-                answers_queue.put(req)
-                return
-            query = found[0]
-            req.success = 1
-            video = video_by_query(query)
-            if video is None:
-                req.response_text = u'К сожалению, по запросу "{}" ничего не найдено'.format(query)
-                req.complete = send_plain_message(req.user_id, req.response_text)
-            else:
-                req.response_text = video
-                req.complete = send_message_video(req.user_id, '', video)
-            answers_queue.put(req)
-            return
-        if u'смена языка' in text:
-            conn = None
-            try:
-                conn = config.MYSQL_POOL.get_connection()
-                update_user_locale(conn, req.user_id, 'eng')
-            except Exception as e:
-                log(str(e))
-                return
-            finally:
-                if conn is not None:
-                    conn.close()
-            req.category = 'Locale'
-            req.type = 'Change'
-            req.response_text = u'Done. Your language is now English'
-            req.success = 1
-            req.complete = send_plain_message(req.user_id, req.response_text)
-            req.save = True
-            answers_queue.put(req)
-            return
-        req.category = 'Undefined'
-        req.type = 'Undefined'
-        req.response_text = u'Неизвестный тип запроса. Попробуйте вызвать команду "помощь"'
-        req.success = 0
-        req.complete = send_plain_message(req.user_id, req.response_text)
-        req.error_message = 'undefined request'
-        req.save = True
-        answers_queue.put(req)
-    except Exception as e:
-        log(str(e))
-        req.success = 0
-        req.error_message = 'error in processing request'
-        req.response_text = u'Ошибка в выполнении запроса'
+        req.response_text = processing_err_message_dict[locale]
         req.complete = send_plain_message(req.user_id, req.answer_text)
         req.save = True
         answers_queue.put(req)
@@ -409,11 +273,7 @@ def process(req):
             conn.close()
 
     text = str(text).decode('utf-8').lower()
-    if locale == 'eng':
-        parse_eng_request(req, text)
-    else:
-        parse_ru_request(req, text)
-
+    parse_request(req, text, locale)
 
 
 
